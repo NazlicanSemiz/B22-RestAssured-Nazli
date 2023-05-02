1c3775f0d53d7bca3cca1ff91b3ef821659da2c9
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-05-02 12:48:53 +0100

    first test case done

--- src/test/java/com/cybertek/Day2/SpartanGetRequests.java
+++ src/test/java/com/cybertek/Day2/SpartanGetRequests.java
@@ -1,14 +1,47 @@
 package com.cybertek.Day2;
 
+import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
+import io.restassured.response.Response;
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.Test;
+
 public class SpartanGetRequests {
 
-    String url = "http://100.26.102.114:8000/api/spartans";
+    String BaseUrl = "http://100.26.102.114:8000";
 
 //  Given Accept type application/json
 //  When user send GET request to api/spartans end point
-//  Then response Content Type must be application/json
+//  Then status code must 200
+//  And response Content Type must be application/json
 //  And response body should be include spartan result
 
+    @Test
+    public void test1(){
+
+        Response response = RestAssured.given().accept(ContentType.JSON)
+                .when()
+                .get(BaseUrl + "/api/spartans");
+
+
+        // printing status code from response object
+        System.out.println("response.statusCode() = " + response.statusCode());
+
+        // printing response content type from response object
+        System.out.println("response.contentType() = " + response.contentType());
+
+        // print whole result body
+        response.prettyPrint();
+
+        // how to do API testing then ?
+        // verify status code is 200
+        Assertions.assertEquals(response.statusCode(),200);
+
+        // verify content type is application/json
+        Assertions.assertEquals(response.contentType(), "application/json");
+
+
+    }
 
 
 }
