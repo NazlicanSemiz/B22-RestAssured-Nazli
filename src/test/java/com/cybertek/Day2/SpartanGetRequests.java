58bdfe7f414827162e6a43d0700ca6e5e55b4022
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-05-02 12:28:39 +0100

    ready for first testcase

---
+++ src/test/java/com/cybertek/Day2/SpartanGetRequests.java
@@ -0,0 +1,14 @@
+package com.cybertek.Day2;
+
+public class SpartanGetRequests {
+
+    String url = "http://100.26.102.114:8000/api/spartans";
+
+//  Given Accept type application/json
+//  When user send GET request to api/spartans end point
+//  Then response Content Type must be application/json
+//  And response body should be include spartan result
+
+
+
+}
