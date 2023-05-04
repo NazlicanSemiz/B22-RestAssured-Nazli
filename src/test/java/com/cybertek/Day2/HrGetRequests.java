e3fe89259f6900238138d87ea7c9e107a33b4266
Nazlican Semiz <nazlicankucuksabanoglu@gmail.com>
2023-05-04 16:10:17 +0100

    task is done

--- src/test/java/com/cybertek/Day2/HrGetRequests.java
+++ src/test/java/com/cybertek/Day2/HrGetRequests.java
@@ -1,11 +1,16 @@
 package com.cybertek.Day2;
 
 import io.restassured.RestAssured;
+import io.restassured.http.ContentType;
 import io.restassured.response.Response;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeAll;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
+
+import static io.restassured.RestAssured.*;
+import static org.junit.jupiter.api.Assertions.*;
+
 
 public class HrGetRequests {
 
@@ -22,7 +27,7 @@ public class HrGetRequests {
     @Test
     public void test1(){
 
-        Response response = RestAssured.get("/regions");
+        Response response = get("/regions");
 
         System.out.println(response.statusCode());
 
@@ -40,18 +45,19 @@ public class HrGetRequests {
     @Test
     public void test2(){
 
-        Response response = RestAssured.get("/regions/2");
+        Response response = given().accept(ContentType.JSON)
+                .when().get("/regions/2");
 
         // verify status code
-        Assertions.assertEquals(200, response.statusCode());
+        assertEquals(200, response.statusCode());
 
         // verify content tpe
-        Assertions.assertEquals("application/json", response.contentType());
+        assertEquals("application/json", response.contentType());
 
         response.prettyPrint();
 
         // veridy body contains Americas
-        Assertions.assertEquals(response.body().asString().contains("Americas"), true);
+        assertTrue(response.body().asString().contains("Americas"));
 
     }
 
